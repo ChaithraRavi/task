32b7b0ec9a7d01ddf0393baa9f5dfe263c64e520
ChaithraRavi <chaithra@vapourmeta.com>
2022-11-25 17:23:53 +0530

    practice

--- login.py
+++ login.py
@@ -1,5 +1,31 @@
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import FastAPI
+from db import user_collection
 
 app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+class middleware:
+    def func(username,password):
+        user = user_collection.find_one({"name": username})
+        if user['password'] == password:
+            return "success"
+        else:
+            return "failed"
+    
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+        
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(
+            print("agds")
+        )
+        
+    @classmethod
+    def validate(cls, v):
+        pass
+
+    def __repr__(self):
+        return f'PostCode({super().__repr__()})'
